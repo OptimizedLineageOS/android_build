0d23ae72409d4aed0b4429c2e053cd542eab91cd
Mattia D'Alleva <antaresone@antaresone.eu>
2015-10-27 02:20:57 +0100

    releasetools: do not dump fingerprint
    Remove that useless "target" string
    
    Change-Id: I592eabb69fb9c4348888be42614a800596b020d9
    
    Conflicts:
    	tools/releasetools/ota_from_target_files.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -677,13 +677,6 @@ reboot_now("%(bcb_dev)s", "recovery");
 else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
-    # Stage 3/3: Make changes.
-    script.Comment("Stage 3/3")
-
-  # Dump fingerprints
-  script.Print("Target: %s" % CalculateFingerprint(
-      oem_props, oem_dict, OPTIONS.info_dict))
-
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
